6de16edea7a6f152e842150775dbd7b08552bb18
Victor <angvictor91@gmail.com>
2021-07-22 14:13:35 +0700

    Initial Commit

---
+++ GameCatalogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameCatalogue
+//
+//  Created by Team SSG on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
